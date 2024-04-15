1eebd940e5ddad4c9350578d454a7af7018d6326
Hamza Tounsi <tounsi.hamza@esprit.tn>
2024-04-15 11:19:01 +0100

    devops

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,11 +3,17 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableScheduling
 @SpringBootApplication
+@RestController
 public class AchatApplication {
-
+@GetMapping("")
+public String getMessage(){
+    return "working...";
+}
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
     }
